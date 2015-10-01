aa92b25c876e11cb85b73fae9b7c2365e50f8131
Leah Steinberg <leahsteinberg1@gmail.com>
2015-10-01 11:27:49 -0400

    changed data type for direction

--- snake.elm
+++ snake.elm
@@ -1,7 +1,7 @@
-import Keyboard
+import Keyboard exposing (arrows)
 import Window
-import Signal exposing (map, foldp)
-import Time exposing (every, second)
+import Signal exposing (map, foldp, merge)
+import Time exposing (every, second, Time)
 import List exposing (head, tail, take, drop, length)
 import Maybe exposing (withDefault)
 import Graphics.Element exposing (image, container, middle, Element)
@@ -11,6 +11,10 @@ import Color exposing (lightRed, white, darkRed)
 -- Constants
 pointDim = 10
 pointPlace = 15
+left = {x = -1, y = 0}
+right = {x = 1, y = 0}
+up = {x = 0, y = 1}
+down = {x = 0, y = -1}
 
 
 --Models
@@ -18,50 +22,74 @@ type alias Snake = (List Dot)
 type Dot = D Dir Point
 
 type alias Point = {x: Int, y: Int}
-type Dir = Up | Down | Left | Right
+type alias Dir = {x: Int, y: Int}--Up | Down | Left | Right
 
-initSnake = [D Left {x= -1, y =0}, D Left {x=0, y= 0}, D Left {x = 1, y = 0}, D Down {x = 2, y =0},
-           D Down {x=2, y = 1}, D Down {x=2, y =2}, D Down {x=2, y =3}]
+
+initSnake = [D left {x= -1, y =0}, D left {x=0, y= 0}, D left {x = 1, y = 0}, D down {x = 2, y =0},
+           D down {x=2, y = 1}, D down {x=2, y =2}, D down {x=2, y =3}]
 
 
 --Update
+
 scooch : Dir -> Point -> Dot
-scooch dir p = 
-  case dir of
-    Up -> D dir {p | y <- p.y + 1}
-    Down -> D dir {p | y <- p.y - 1}
-    Left -> D dir {p | x <- p.x - 1}
-    Right -> D dir {p | x <- p.x + 1}
+scooch dir p =
+  D dir {x = p.x + dir.x,  y = p.y + dir.y }
+
 
 
 
 slither : Snake -> Snake
-slither snake = 
-  let front = withDefault (D Up {x=0, y=0}) (head snake)
+slither snake =
+  let front = withDefault (D up {x=10, y=10}) (head snake)
       middle = take (length snake - 2) (withDefault ([]) (tail snake))
-      back = withDefault (D Up {x=0, y=0}) (head (drop (length snake - 1) snake))
+      back = withDefault (D up {x=0, y=0}) (head (drop (length snake - 1) snake))
   in
      case front of
        D dir p -> [scooch dir p] ++ [front] ++ middle
+
+--moveSnake : Update -> Snake -> Snake
+--move update snake = 
+--  case update of
+--      Tick t -> slither snake
+--      Arrow dir -> turn dir snake
+
+--turn : {x: Int, y: Int} -> Snake -> Snake
+--turn dir snake = 
+--  let front = withDefault (D Up {x=0, y=0}) (head snake)
+--      body =  (withDefault ([]) (tail snake))
+--  in
+--     snake
+--     case front of
+--       D dir point -> [D ]
+
+
 
 
 --View
 
 drawDot : Dot -> Form
-drawDot dot = 
+drawDot dot =
   let makePoint p = (toFloat p.x * pointPlace, toFloat p.y * pointPlace)
       drawPoint colour p = rect pointDim pointDim
                       |> filled colour
                       |> move (makePoint p)
   in
-      case dot of 
+      case dot of
         D dir p ->
                 drawPoint lightRed p
+
+type Update = Tick Time | Arrow Dir
+
+--updates : Signal Update
+--updates =
+--    merge
+--    (map Tick (every second))
+--    (map Arrow arrows)
 
 
 drawSnake snake = List.map drawDot snake
 
 display snake = collage 500 500 (drawSnake snake)
 -- fold p: (a -> state -> state) -> initState -> Signal a -> Signal State
-x = foldp (\tick snake -> slither snake) initSnake (every second)
-main  = map display (foldp (\tick snake -> slither snake) initSnake (every second))
+--x = foldp (\tick snake -> slither snake) initSnake (every second)
+main  = map display (foldp (\change snake -> slither snake) initSnake (every second))
