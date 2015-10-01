8375d8edb3b2006498f92344bdeaa833ee76ea63
Leah Steinberg <leahsteinberg1@gmail.com>
2015-10-01 16:03:19 -0400

    goes blank when game over and draws a single food

--- snake.elm
+++ snake.elm
@@ -2,11 +2,11 @@ import Keyboard exposing (arrows)
 import Window
 import Signal exposing (map, foldp, merge)
 import Time exposing (every, second, Time, fps)
-import List exposing (head, tail, take, drop, length)
+import List exposing (head, tail, take, drop, length, foldr)
 import Maybe exposing (withDefault)
 import Graphics.Element exposing (image, container, middle, Element)
 import Graphics.Collage exposing (collage, rect, ngon, filled, move, rotate, toForm, Form)
-import Color exposing (lightRed, white, darkRed)
+import Color exposing (lightRed, white, darkRed, Color)
 
 -- Constants
 pointDimension = 10
@@ -24,7 +24,10 @@ type Dot = D Dir Point
 
 type alias Point = {x: Int, y: Int}
 type alias Dir = {x: Int, y: Int}--Up | Down | Left | Right
+type alias Food = {x: Int, y: Int}
+type alias State = (Food, Snake)
 
+initFood = {x=10, y=10}
 
 initSnake = [D left {x= -1, y =0}, D left {x=0, y= 0}, D left {x = 1, y = 0}, D down {x = 2, y =0},
            D down {x=2, y = 1}, D down {x=2, y =2}, D down {x=2, y =3}]
@@ -32,10 +35,30 @@ initSnake = [D left {x= -1, y =0}, D left {x=0, y= 0}, D left {x = 1, y = 0}, D 
 
 --Update
 wrap : Int -> Int
-wrap coord = 
-  if |coord < gridDimension/2 -> gridDimension // 2
-     |coord >= gridDimension/2 -> -gridDimension // 2
+wrap coord =
+  if |coord <  -gridDimension//2 -> gridDimension//2
+     |coord >= gridDimension//2 -> -gridDimension // 2
      |otherwise -> coord
+
+
+getPoint : Dot -> Point
+getPoint dot =
+  case dot of
+    D dir p -> p
+
+getDir : Dot -> Dir
+getDir dot =
+  case dot of
+    D dir p -> dir
+
+
+checkCollide : Dot -> Snake -> Bool
+checkCollide dot snake =
+  let collides point1 point2 = if point1.x == point2.x && point1.y == point2.y then True else False
+  in
+     case dot of
+       D newDot newP ->
+         foldr (\snakeDot accum -> if not accum then accum else not (collides newP (getPoint snakeDot))) True snake
 
 
 scooch : Dir -> Point -> Dot
@@ -53,11 +76,14 @@ slither snake =
       back = withDefault (D up {x=0, y=0}) (head (drop (length snake - 1) snake))
   in
      case front of
-       D dir p -> [scooch dir p] ++ [front] ++ middle
+       D dir p ->
+         let newDot = scooch dir p
+         in
+            if checkCollide newDot middle then [newDot] ++ [front] ++ middle else []
 
 
 turn : {x: Int, y: Int} -> Snake -> Snake
-turn arrowDir snake = 
+turn arrowDir snake =
     let front = withDefault (D up {x=0, y=0}) (head snake)
         body =  (withDefault ([]) (tail snake))
     in
@@ -72,28 +98,36 @@ noBackwards arrow head =
        |otherwise -> arrow
 
 moveSnake : Update -> Snake -> Snake
-moveSnake update snake = 
+moveSnake update snake =
   case update of
       Tick t -> slither snake
       Arrow dir -> turn dir snake
 
 
 --View
-drawDot : Dot -> Form
-drawDot dot =
-  let makePoint p = (toFloat p.x * pointPlace, toFloat p.y * pointPlace)
-      drawPoint colour p = rect pointDimension pointDimension
+
+
+renderPoint p = (toFloat p.x * pointPlace, toFloat p.y * pointPlace)
+
+drawPoint : Color -> Point -> Form
+drawPoint colour p = rect pointDimension pointDimension
                       |> filled colour
-                      |> move (makePoint p)
-  in
+                      |> move (renderPoint p)
+
+
+
+drawDot :  Color -> Dot -> Form
+drawDot col dot=
       case dot of
         D dir p ->
-                drawPoint lightRed p
+                drawPoint col p
+
+drawSnakeDot = drawDot lightRed
 
 type Update = Tick Time | Arrow Dir
 
 isDirectionArrow : Dir -> Bool
-isDirectionArrow dir = 
+isDirectionArrow dir =
   if dir.x == 0 && dir.y == 0 then False else True
 
 
@@ -104,9 +138,16 @@ updates =
     in
         merge
         (map Tick (every second))
-        (map Arrow (Signal.filter isDirectionArrow up (Signal.sampleOn delta Keyboard.arrows))) 
+        (map Arrow (Signal.filter isDirectionArrow up (Signal.sampleOn delta Keyboard.arrows)))
 
-drawSnake snake = List.map drawDot snake
 
-display snake = collage (gridDimension * pointPlace) (gridDimension * pointPlace) (drawSnake snake)
+
+drawSnake snake = List.map drawSnakeDot snake
+
+display snake = collage (gridDimension * pointPlace) (gridDimension * pointPlace) ([drawPoint darkRed initFood] ++ (drawSnake snake))
 main = map display (foldp (\update snake -> moveSnake update snake) initSnake updates)
+
+
+
+
+
