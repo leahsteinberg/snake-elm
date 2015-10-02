8bf00035ead7228664a552a0adb359c9b55f0554
Leah Steinberg <leahsteinberg1@gmail.com>
2015-10-02 15:47:27 -0400

    generates random yummy foods

--- snake2.elm
+++ snake2.elm
@@ -7,6 +7,7 @@ import Maybe exposing (withDefault)
 import Graphics.Element exposing (image, container, middle, Element)
 import Graphics.Collage exposing (collage, rect, ngon, filled, move, rotate, toForm, Form)
 import Color exposing (lightRed, white, darkRed, Color)
+import Random exposing (int, generate, Seed, Generator, initialSeed)
 
 
 -- Model
@@ -16,9 +17,7 @@ type alias Vertebra = {point: Point, dir: Dir}
 type alias Point = {x: Int, y: Int}
 type alias Dir = {x: Int, y: Int}
 type alias Food = Point
-type alias State = (Food, Snake)
-
-  
+type alias State = (Food, Snake, Seed)
 type Update = Tick Time | Arrow Dir
 
 pointDim = 10
@@ -26,8 +25,8 @@ pointOffset = 15
 gridDim = 46
 defaultVert = {point= {x=0, y=0}, dir= {x=1, y =0}}
 upArrow = {x=0, y =1}
-
 initFood = {x= 5, y = 5}
+newFood = {x= -5, y = -5}
 
 initSnake = [
   {point= {x= -1, y=0}, dir= {x= -1, y= 0}}
@@ -38,7 +37,11 @@ initSnake = [
   ,{point= {x= 1, y=3}, dir= {x= 0, y= -1}}
   ,{point= {x= 2, y=3}, dir= {x= -1, y= 0}}]
 
-initState = (initFood, initSnake)
+--initSe = int -13 13
+s : Int -> Seed
+s nnnn= initialSeed 5
+
+initState = (initFood, initSnake, initialSeed 5)
 
 -- Helpers
 getFront : Snake -> Vertebra
@@ -67,8 +70,7 @@ noBackwards arrowDir vert =
      |arrowDir.y == 0 && vert.dir.y == 0 -> vert.dir
      |otherwise -> arrowDir
 
-newFood : Food
-
+--newFood : Food
 
 -- Update
 
@@ -83,20 +85,20 @@ updates =
      (map Arrow (filter isDirectionArrow upArrow (sampleOn delta arrows)))
 
 updateState : Update -> State -> State
-updateState update state = 
-  let (food, snake) = state
+updateState update state =
+  let (food, snake, seed) = state
   in
       case update of
         Tick s -> slither state
-        Arrow dir -> (food, turn dir snake)
+        Arrow dir -> (food, turn dir snake, seed)
 
 slither : State -> State
 slither state =
-  let (food, snake) = state
+  let (food, snake,seed) = state
       newVert = scooch (getFront snake)
       snakeMiddle = getMiddle snake
   in
-     if (eatsOwnTail newVert snake) then (food, []) else nibble (food, [newVert] ++ [getFront snake] ++ snakeMiddle)
+     if (pointOnSnake newVert.point snake) then (food, [], seed) else nibble (food, [newVert] ++ [getFront snake] ++ snakeMiddle, seed)
 
 turn : Dir -> Snake -> Snake
 turn arrowDir snake =
@@ -108,15 +110,16 @@ turn arrowDir snake =
 
 
 addTail : Snake -> Snake
-addTail snake = 
+addTail snake =
   let back = getBack snake
-      newBack = {back | point <- {x = back.point.x - back.dir.x, y = back.point.y - back.dir.y}}
+      newBack = {back | point <- {x = back.point.x - back.dir.x,
+                y = back.point.y - back.dir.y}}
   in
      snake ++ [newBack]
 
 
 scooch : Vertebra -> Vertebra
-scooch front = 
+scooch front =
   {
     point = {x = front.point.x + front.dir.x, y = front.point.y + front.dir.y},
     dir = front.dir
@@ -126,20 +129,28 @@ scooch front =
 chomp : {a | x: Int, y: Int} -> {a | x: Int, y: Int} -> Bool
 chomp fangs morsel = fangs.x == morsel.x && fangs.y == morsel.y
 
-eatsOwnTail : Vertebra -> Snake -> Bool
-eatsOwnTail newVert snake =
+pointOnSnake : Point -> Snake -> Bool
+pointOnSnake newPoint snake =
   let oldSnake = [getFront snake] ++ (getMiddle snake)
   in
-     foldr (\ vert accum -> if chomp newVert.point vert.point then True else accum) False oldSnake
+     foldr (\vert accum -> if chomp newPoint vert.point then True else accum) False oldSnake
+
+digest : State -> State
+digest state =
+  let (food, snake, seed) = state
+      (newInt, newSeed) = generate (int -13 13) seed
+      (newerInt, newerSeed) = generate (int -13 13) newSeed
+      newFood = {x = newInt, y = newerInt}
+  in
+     if not (pointOnSnake newFood snake) then (newFood, snake, newerSeed) else digest (food, snake, newerSeed)
 
 
 nibble : State -> State
-nibble state = 
-  let (food, snake) = state
+nibble state =
+  let (food, snake, seed) = state
       frontVert = getFront snake
   in
-     if chomp food frontVert.point then (newFood, addTail snake) else (food, snake)
-
+     if chomp food frontVert.point then digest state else state
 -- View
 
 drawPoint : Color -> {a | x: Int, y: Int} -> Form
@@ -149,9 +160,9 @@ drawPoint color point = rect pointDim pointDim
 
 displayState : State -> Element
 displayState state =
-  let (food, snake) = state
+  let (food, snake, seed) = state
       pixelDim = gridDim * pointOffset
-  in 
+  in
      collage pixelDim pixelDim ([drawPoint darkRed food] ++ (List.map (\vert -> (drawPoint lightRed) vert.point) snake))
 
 main = map displayState (foldp (\update state -> updateState update state) initState updates)
